8cc54c173bd158b9512fecccc781a18dae7fe0fc
Parsa Shadab <shadab.balgori@gmail.com>
2021-10-08 13:14:46 +0330

    Fixed code style

--- repo-backup.sh
+++ repo-backup.sh
@@ -9,81 +9,81 @@
 
 #Give input from user
 function init(){
-  if [[ -f .profile_git ]]
-  then
-    user=`cat .profile_git | cut -d ':' -f 1 `
-    organization=`cat .profile_git | cut -d ':' -f 2 `
-    token=`cat .profile_git | cut -d ':' -f 3 `
-    echo "This is your stored information:" ; sleep 1
-    echo User \: $user ; sleep 0.5
-    echo Organization name \: $organization ; sleep 0.5
-    echo GitHub access token \: $token ; sleep 2
-    State=1
-    while [[ $State = 1 ]]
-    do 
-      read -p 'Are these data true? ([Y]es, [N]o) ' data_verify
-      if [[ $data_verify = 'yes' ]] || [[ $data_verify = 'y' ]] || [[ $data_verify = 'Y' ]]
-		  then
-		    echo "Got it!" ; sleep 1
-        State=0
-		  elif [[ $data_verify = 'no' ]] || [[ $data_verify = 'n' ]] || [[ $data_verify = 'N' ]]
-	    then
-		    echo "Don't worry. We'll fix it! :)" ; rm .profile_git ; sleep 1
+    if [[ -f .profile_git ]]
+    then
+        user=`cat .profile_git | cut -d ':' -f 1 `
+        organization=`cat .profile_git | cut -d ':' -f 2 `
+        token=`cat .profile_git | cut -d ':' -f 3 `
+        echo "This is your stored information:" ; sleep 1
+        echo User \: $user ; sleep 0.5
+        echo Organization name \: $organization ; sleep 0.5
+        echo GitHub access token \: $token ; sleep 2
+        State=1
+        while [[ $State = 1 ]]
+        do 
+            read -p 'Are these data true? ([Y]es, [N]o) ' data_verify
+            if [[ $data_verify = 'yes' ]] || [[ $data_verify = 'y' ]] || [[ $data_verify = 'Y' ]]
+            then
+                echo "Got it!" ; sleep 1
+            State=0
+            elif [[ $data_verify = 'no' ]] || [[ $data_verify = 'n' ]] || [[ $data_verify = 'N' ]]
+            then
+                echo "Don't worry. We'll fix it! :)" ; rm .profile_git ; sleep 1
+                read -p 'Please enter your GitHub username: ' user
+                read -p 'Please enter your GitHub organization name: ' organization
+                read -p 'Please enter your GitHub token : ' token
+                read -p 'Do you want store this information for future usage? ([Y]es, [N]o) ' store
+                if [[ $store = 'yes' ]] || [[ $store = 'y' ]] || [[ $store = 'Y' ]]
+                then
+                    echo $user:$organization:$token > .profile_git ; sleep 1 ; State=0
+                elif [[ $store = 'no' ]] || [[ $store = 'n' ]] || [[ $store = 'N' ]]
+                then
+                    echo "OK!" ; sleep 1 ; State=0
+                else
+                    echo "Operation not specified!" ; sleep 0.5
+                    echo "Please try again!" ; sleep 0.5
+                fi
+            else
+                echo "Operation not specified!" ; sleep 0.5
+                echo "Please try again!" ; sleep 0.5
+            fi
+        done
+    else
+        echo This script need your information to perform backup task ; sleep 1 
         read -p 'Please enter your GitHub username: ' user
         read -p 'Please enter your GitHub organization name: ' organization
         read -p 'Please enter your GitHub token : ' token
-        read -p 'Do you want store this information for future usage? ([Y]es, [N]o) ' store
-        if [[ $store = 'yes' ]] || [[ $store = 'y' ]] || [[ $store = 'Y' ]]
-	      then
-		  	  echo $user:$organization:$token > .profile_git ; sleep 1 ; State=0
-	      elif [[ $store = 'no' ]] || [[ $store = 'n' ]] || [[ $store = 'N' ]]
-	      then
-		      echo "OK!" ; sleep 1 ; State=0
-	      else
-		      echo "Operation not specified!" ; sleep 0.5
-		      echo "Please try again!" ; sleep 0.5
-	      fi
-      else
-        echo "Operation not specified!" ; sleep 0.5
-        echo "Please try again!" ; sleep 0.5
-      fi
-    done
-  else
-    echo This script need your information to perform backup task ; sleep 1 
-    read -p 'Please enter your GitHub username: ' user
-    read -p 'Please enter your GitHub organization name: ' organization
-    read -p 'Please enter your GitHub token : ' token
-    State=1
-    while [[ $State = 1 ]]
-    do
-      read -p 'Do you want store this information for future usage? ([Y]es, [N]o) ' store
-		  if [[ $store = 'yes' ]] || [[ $store = 'y' ]] || [[ $store = 'Y' ]]
-		  then
-		  	echo $user:$organization:$token > .profile_git ; sleep 1 ; State=0
-		  elif [[ $store = 'no' ]] || [[ $store = 'n' ]] || [[ $store = 'N' ]]
-		  then
-		  	echo "OK!" ; sleep 1 ; State=0			
-		  else
-		  	echo "Operation not specified!" ; sleep 0.5
-		  	echo "Please try again!" ; sleep 0.5
-		  fi
-    done
-  fi
+        State=1
+        while [[ $State = 1 ]]
+        do
+            read -p 'Do you want store this information for future usage? ([Y]es, [N]o) ' store
+            if [[ $store = 'yes' ]] || [[ $store = 'y' ]] || [[ $store = 'Y' ]]
+		    then
+		        echo $user:$organization:$token > .profile_git ; sleep 1 ; State=0
+		    elif [[ $store = 'no' ]] || [[ $store = 'n' ]] || [[ $store = 'N' ]]
+		    then
+		  	    echo "OK!" ; sleep 1 ; State=0			
+		    else
+		  	    echo "Operation not specified!" ; sleep 0.5
+		  	    echo "Please try again!" ; sleep 0.5
+		    fi
+        done
+    fi
 }
 
 #Give each repository and archive them
 function file_perform(){
-  for repo in `curl -s https://api.github.com/orgs/$organization/repos\?type\=private\&per_page\=100 -u ${user}:${token} | grep 'ssh_url' | sed -e 's/"//g' -e 's/ *//g' -e 's/,//' | cut -d ':' -f 2,3`
-  do
-    repo_name=$(echo $repo | cut -d '/' -f 2 | cut -d '.' -f 1)
-    echo "Repository found: $repo_name ($repo)"
-    git clone $repo
-    echo archiving repository $repo_name...
-    tar cJvf $repo_name.tar.xz $repo_name
-    rm -rf $repo_name
-    echo '======================================================'
-    sleep 1
-  done
+    for repo in `curl -s https://api.github.com/orgs/$organization/repos\?type\=private\&per_page\=100 -u ${user}:${token} | grep 'ssh_url' | sed -e 's/"//g' -e 's/ *//g' -e 's/,//' | cut -d ':' -f 2,3`
+    do
+        repo_name=$(echo $repo | cut -d '/' -f 2 | cut -d '.' -f 1)
+        echo "Repository found: $repo_name ($repo)"
+        git clone $repo
+        echo archiving repository $repo_name...
+        tar cJvf $repo_name.tar.xz $repo_name
+        rm -rf $repo_name
+        echo '======================================================'
+        sleep 1
+    done
 }
 
 
