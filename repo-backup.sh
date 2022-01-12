41f2ba1cc8664f797a737316fff97bfa8744054c
Parsa Shadab <shadab.balgori@gmail.com>
2022-01-12 15:26:25 +0330

    Improved script performance

--- repo-backup.sh
+++ repo-backup.sh
@@ -21,10 +21,10 @@ function get_store_data(){
 		    echo $user:$organization:$token > .profile_git ; sleep 1 ; GSstate=0
 		elif [[ $store = 'no' ]] || [[ $store = 'n' ]] || [[ $store = 'N' ]]
 		then
-		    echo "OK!" ; sleep 1 ; GSstate=0			
+		    custom_echo1 "OK!" 1 ; GSstate=0			
 		else
-		  	echo "Operation not specified!" ; sleep 0.5
-		  	echo "Please try again!" ; sleep 0.5
+		  	custom_echo1 "Operation not specified!" 0.5
+		  	custom_echo1 "Please try again!" 0.5
 		fi
     done
 }
@@ -35,29 +35,29 @@ function init(){
         user=`cat .profile_git | cut -d ':' -f 1 `
         organization=`cat .profile_git | cut -d ':' -f 2 `
         token=`cat .profile_git | cut -d ':' -f 3 `
-        echo "This is your stored information:" ; sleep 1
-        echo User \: $user ; sleep 0.5
-        echo Organization name \: $organization ; sleep 0.5
-        echo GitHub access token \: $token ; sleep 2
+        custom_echo1 "This is your stored information:" 1
+        custom_echo1 "User : $user" 0.5
+        custom_echo1 "Organization name : $organization" 0.5
+        custom_echo1 "GitHub access token : $token" 2
         State=1
         while [[ $State = 1 ]]
         do 
             read -p 'Are these data true? ([Y]es, [N]o) ' data_verify
             if [[ $data_verify = 'yes' ]] || [[ $data_verify = 'y' ]] || [[ $data_verify = 'Y' ]]
             then
-                echo "Got it!" ; sleep 1
+                custom_echo1 "Got it!" 1
                 State=0
             elif [[ $data_verify = 'no' ]] || [[ $data_verify = 'n' ]] || [[ $data_verify = 'N' ]]
             then
-                echo "Don't worry. We'll fix it! :)" ; rm .profile_git ; sleep 1
+                custom_echo1 "Don't worry. We'll fix it! :)" 1 ; rm .profile_git
                 get_store_data ; State=0
             else
-                echo "Operation not specified!" ; sleep 0.5
-                echo "Please try again!" ; sleep 0.5
+                custom_echo1 "Operation not specified!" 0.5
+		  	    custom_echo1 "Please try again!" 0.5
             fi
         done
     else
-        echo This script need your information to perform backup task ; sleep 1 
+        custom_echo1 "This script need your information to perform backup task" 1 
         get_store_data
     fi
 }
@@ -69,23 +69,26 @@ function file_perform(){
         repo_name=$(echo $repo | cut -d '/' -f 2 | cut -d '.' -f 1)
         echo "Repository found: $repo_name ($repo)"
         git clone $repo
-        echo archiving repository $repo_name...
+        custom_echo2 1 "Archiving repository $repo_name..."
         tar cJvf $repo_name.tar.xz $repo_name
         rm -rf $repo_name
-        echo '======================================================'
-        sleep 1
+        custom_echo1 '======================================================' 1
     done
 }
 
+#Echo + sleep commmand mixture
+function custom_echo1(){
+    echo $1 ; sleep $2
+}
+function custom_echo2(){
+    sleep $1 ; echo $2
+}
 
 #Main code
 clear
-sleep 1
-echo "Welcome to Organization Backup scrpit!"
-sleep 1
-echo "Developed by PS-Professional"
-sleep 1
-echo "Let's start! :)"
+custom_echo2 1 "Welcome to Organization Backup scrpit!"
+custom_echo2 1 "Developed by PS-Professional"
+custom_echo2 1 "Let's start! :)"
 init
 
 mkdir tmp ; cd tmp ; pwd
@@ -93,6 +96,5 @@ file_perform
 tar cJvf tmp * && mv tmp ../RepoBackup\(`date +"%d%b%Y"`\).tar.xz
 rm -f * && cd .. && rm -rf tmp
 
-echo "Final file created!"
-sleep 1
-echo "Have a nice day! :)"
+custom_echo2 1 "Final file created!"
+custom_echo2 1 "Have a nice day! :)"
